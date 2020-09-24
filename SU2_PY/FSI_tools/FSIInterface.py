d4c3e68cb93560f65ee7c786c488a0eb4a193e4d
Nicola-Fonzi <nicola.fonzi@polimi.it>
2020-09-24 13:24:49 +0200

    debug 3

--- SU2_PY/FSI_tools/FSIInterface.py
+++ SU2_PY/FSI_tools/FSIInterface.py
@@ -1945,8 +1945,6 @@ class Interface:
             if myid in self.solidSolverProcessors:
               SolidSolver.updateSolution()
             FluidSolver.Update()
-            FluidSolver.Monitor(TimeIter)
-            FluidSolver.Output(TimeIter)
           #If no restart
           else:
             self.MPIPrint('Setting FSI initial conditions')
