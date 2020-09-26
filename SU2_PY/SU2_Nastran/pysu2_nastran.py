1831eec0719657b1f768ddb90caa310e21c8a682
Nicola-Fonzi <nicola.fonzi@polimi.it>
2020-09-26 14:30:10 +0200

    more info about the modal forces

--- SU2_PY/SU2_Nastran/pysu2_nastran.py
+++ SU2_PY/SU2_Nastran/pysu2_nastran.py
@@ -552,6 +552,9 @@ class Solver:
       #self.q[0]=((3.0*pi/180.0)+(1.0*pi/180.0)*sin(2*pi*10.0*time))/4.796908e-01
       #self.qdot[0] = 2.0*pi*10.0*((1.0*pi/180.0)*cos(2*pi*10.0*time))/4.796908e-01
       #self.qddot[0] = -4.0*pi*pi*100.0*((1.0*pi/180.0)*sin(2*pi*10.0*time))/4.796908e-01
+      #self.q[1]= -pi/180.0*sin(2.0*pi*8.0*time)
+      #self.qdot[1] = -pi/180.0*cos(2.0*pi*8.0*time)*(2.0*pi*8.0)
+      #self.qddot[1] = pi/180.0*sin(2.0*pi*8.0*time)*(2.0*pi*8.0)*(2.0*pi*8.0)
       self.q[self.Config["IMPOSED_MODE"]] = eval(self.Config["IMPOSED_DISP"])
       self.qdot[self.Config["IMPOSED_MODE"]] = eval(self.Config["IMPOSED_VEL"])
       self.qddot[self.Config["IMPOSED_MODE"]] = eval(self.Config["IMPOSED_ACC"])
@@ -572,6 +575,8 @@ class Solver:
       FY[iPoint] = float(Force[1])
       FZ[iPoint] = float(Force[2])
     self.F = self.UxT.dot(FX) + self.UyT.dot(FY) + self.UzT.dot(FZ)
+    print("The modal forces are:")
+    print(self.F)
 
   def __ComputeResidual(self):
     """ Description. """
