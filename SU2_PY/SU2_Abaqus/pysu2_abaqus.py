83f82b57cb66b38cb1c343c4bc690a5c86efcfa1
vittocavalieri <vittorio.cavalieri@polimi.it>
2022-09-13 10:00:01 +0200

    Add RESTART_SOL option

--- SU2_PY/SU2_Abaqus/pysu2_abaqus.py
+++ SU2_PY/SU2_Abaqus/pysu2_abaqus.py
@@ -125,6 +125,7 @@ class Solver:
              (this_param == "INP_FILE") or \
              (this_param == "GENERATOR_FILE") or \
              (this_param == "DATA_FILE") or \
+             (this_param == "RESTART_SOL") or \
              (this_param == "MOVING_MARKER") or \
              (this_param == "PART_NAME") or \
              (this_param == "SET_NAME"):
