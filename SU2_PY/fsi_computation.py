40cdbeed37521541a5ef061db639bd44da27d71d
Nicola-Fonzi <nicola.fonzi@outlook.com>
2023-09-30 21:17:25 +0200

    Finalize

--- SU2_PY/fsi_computation.py
+++ SU2_PY/fsi_computation.py
@@ -212,7 +212,7 @@ def main():
     comm.barrier()
 
   # --- Exit cleanly the fluid and solid solvers --- #
-  FluidSolver.Postprocessing()
+  FluidSolver.Finalize()
   if myid == rootProcess:
       SolidSolver.exit()
 
